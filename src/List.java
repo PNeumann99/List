cf722452b57f1616a7425805c9371af761c61437
Paul <p.neumann76@gmail.com>
2021-05-16 19:52:43 +0200

    main methode weitestgehend fertig. neue get() methode neue insert() methode
    kleine fixes bei delete methoden bzgl. size
    
    BUGS/TO-DO:
    Nutzer Eingaben müssen noch auf richtigkeit überprüft werden
    get() methode ist noch nicht bugfrei (nullpointer exception)
    get nutzeraktion prüfen: wenn nutzer auf index außerhalb der liste zugreifen will
    delete nutzeraktion prüfen: index außerhalb
    insert nutzeraktion prüfen: index außerhalb, dann einfach korrigieren mit hinweis und append nutzen

--- src/List.java
+++ src/List.java
@@ -14,28 +14,81 @@ public class List<T extends Comparable<T>> {
     }
 
     public static void main(String[] args) throws IOException {
+        // die Liste die der Nutzer bearbeitet initialisieren
+        List<Integer> liste = new List<>();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         boolean keepGoing = true;
+        System.out.println("Es wurde eine leere Liste erstellt.");
         while(keepGoing){
+            System.out.println("Welche Aktion soll durchgeführt werden?\nEingabe: ");
             String input = in.readLine();
-            if (input.equals("e")){
-                keepGoing = false;
-            } else if (input.equals("a")){
+            switch (input) {
+                case "e":  // exit : Programm verlassen
+                    System.out.println("Aktion: exit\nDas Programm wird beendet.");
+                    keepGoing = false;
+                    break;
 
-            } else if (input.equals("i")){
+                case "a":  // append : Nutzer nach Wert fragen und am Ende der Liste einfügen
+                    System.out.println("Aktion: append\nWelchen Wert hat das einzufügende Element?\nEingabe: ");
+                    liste.append(Integer.parseInt(in.readLine()));
+                    System.out.println("Element mit Wert " + liste.tail.data + " wurde am Ende der Liste eingefügt.");
+                    break;
 
-            } else if (input.equals("d")){
+                case "i":  // insert : Nutzer nach Wert und Position fragen und dann dort einfügen
+                    System.out.println("Aktion: insert\nWelchen Wert hat das einzufügende Element?\nEingabe: ");
+                    int data = Integer.parseInt(in.readLine());
+                    System.out.println("An welcher Position soll das Element eingefügt werden?\nEingabe: ");
+                    int pos = Integer.parseInt(in.readLine());
+                    if(pos == liste.size) liste.append(data);
+                    else liste.insert(data, pos);
+                    System.out.println("Ein neues Element mit dem Wert " + data+" wurde an Position "+pos+" in die Liste eingefügt.");
+                    break;
 
-            } else if (input.equals("g")){
+                case "d":  // delete : Nutzer nach Position fragen und Element löschen
+                    System.out.println("Aktion: delete\nAn welcher Position soll das Element gelöscht werden?\n Eingabe: ");
+                    pos = Integer.parseInt(in.readLine());
+                    if (pos == liste.size-1) liste.deleteLast();
+                    else liste.delete(pos);
+                    System.out.println("Das Element an Position "+pos+" wurde gelöscht.");
+                    break;
 
-            } else if (input.equals("r")){
+                case "g":  // get : Nutzer nach Position fragen und Wert des Elementes ausgeben
+                    if(!liste.empty()) {
+                        System.out.println("Aktion: get\nVon welchem Element (Position in d. Liste) soll der Wert ausgegeben werden?\nEingabe: ");
+                        pos = Integer.parseInt(in.readLine());
+                        data = liste.get(pos);
+                        System.out.println("Das Element an Position " + pos + " hat den Wert" + data + ".");
+                    } else { // falls die Liste noch leer ist.
+                        System.out.println("Aktion: get\nDie Liste ist leer!");
+                    }
+                    break;
 
-            } else if (input.equals("p")){
+                case "r":  // reverse : Liste umkehren
+                    System.out.println("Aktion: reverse\nDie Liste wurde umgekehrt.");
+                    liste.reverse();
+                    break;
 
-            } else {
-                System.out.println("Invalid input! Possible operations:\na(ppend)\ni(nsert)\nd(elete)\ng(et)\nr(everse)\np(rint)\ne(xit)");
+                case "p":  // print : Liste auf dem Bildschirm ausgeben
+                    System.out.println("Aktion: print\n Die Liste: "+liste.toString());
+                    break;
+
+                default:
+                    System.out.println("Ungültige Eingabe! Mögliche Aktionen:\na(ppend)\ni(nsert)\nd(elete)\ng(et)\nr(everse)\np(rint)\ne(xit)");
+                    break;
             }
         }
+    }
+
+    // gibt den Wert des Elementes an Position k zurück.
+    public T get(int k){
+        Node<T> current = this.head;
+        int i = 0;
+        while(current != null && current.next != null && i < k){
+            current = current.next;
+            i++;
+        }
+        if(current.next != null) return current.next.data;
+        else return null;
     }
 
     // ist genau dann true, wenn die Liste leer ist. Laufzeit O(1).
@@ -68,10 +121,22 @@ public class List<T extends Comparable<T>> {
         }
     }
 
+    // fügt ein neues Element mit Wert t an Position k ein.
+    public void insert(T t, int k){
+        Node<T> current = this.head;
+        for(int i = 0; i < k; i++){
+            current = current.next;
+        }
+        Node<T> newElement = new Node<>(t);
+        newElement.next = current.next;
+        current.next = newElement;
+        this.size++;
+    }
+
     // löscht das Element am Ende der Liste. Laufzeit O(1).
     public void deleteLast() {
         this.tail = null;
-        this.size -= 1;
+        this.size--;
 
         /* in diesem Moment ist mit einer Laufzeit von O(1) das letzte Element zwar gelöscht,
         jedoch muss der pointer auf den tail neu gesetzt werden. In einfach verketteten Listen ist das Löschen
@@ -128,6 +193,7 @@ public class List<T extends Comparable<T>> {
         // falls der head gelöscht werden soll
         if (k == 0) {
             head = current.next;
+            this.size--;
             return;
         }
 
@@ -144,6 +210,7 @@ public class List<T extends Comparable<T>> {
         // einen pointer auf die node nach der zu löschenden initialisieren
         Node<T> next = current.next.next;
         current.next = next; // die zu löschende node aus der liste werfen
+        this.size--;
 
     }
 
@@ -157,11 +224,14 @@ public class List<T extends Comparable<T>> {
         }
 
         for (Node<T> current = head; current != null; current = current.next) {
+            if (current.next == null) this.tail = current;
             while (current != null && current.next.data.compareTo(t) < 0) {
                 current.next = current.next.next;
                 deleted++;
             }
         }
+        this.size -= deleted;
+
         return deleted;
     }
 }
